395b6cf45b2f2b69e68a4e3f81f2ae3dfa59707d
Vladislav Kavazov <vladislav.kavazov@scalefocus.com>
2019-03-15 23:38:19 +0200

    add shopping-list components

--- ng7-app/src/app/app.module.ts
+++ ng7-app/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './header/header.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
     HeaderComponent,
     RecipeListComponent,
     RecipeDetailComponent,
-    RecipeItemComponent
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
