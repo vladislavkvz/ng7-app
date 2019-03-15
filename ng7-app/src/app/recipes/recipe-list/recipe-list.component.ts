aab93b34279f0472616bd060d404b96d5b67e7ef
Vladislav Kavazov <vladislav.kavazov@scalefocus.com>
2019-03-16 00:30:03 +0200

    add code to show two recipes

--- ng7-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng7-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Name',
+      'Test Description of my recipe',
+      'https://thestayathomechef.com/wp-content/uploads/2018/02/Best-Brownies-1-small.jpg'
+    ),
+    new Recipe(
+    'Test Name',
+    'Test Description of my recipe',
+    'https://thestayathomechef.com/wp-content/uploads/2018/02/Best-Brownies-1-small.jpg'
+    )
+  ];
 
   constructor() { }
 
