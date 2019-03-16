4206f9627629173b39c43a4c89e6509dd248d2e2
Vladislav Kavazov <vladislav.kavazov@scalefocus.com>
2019-03-16 15:36:03 +0200

    add ingredients to recipes

--- ng7-app/src/app/recipes/recipe.service.ts
+++ ng7-app/src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter, Output } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   @Output() recipeSelected = new EventEmitter<Recipe>();
@@ -8,13 +9,21 @@ export class RecipeService {
         new Recipe(
           'Test Name 1',
           'Test Description 1 of my recipe',
-          'https://thestayathomechef.com/wp-content/uploads/2018/02/Best-Brownies-1-small.jpg'
-        ),
+          'https://thestayathomechef.com/wp-content/uploads/2018/02/Best-Brownies-1-small.jpg',
+        [
+          new Ingredient('chocolate', 1),
+          new Ingredient('eggs', 7),
+          new Ingredient('flour', 200)
+        ]),
         new Recipe(
         'Test Name 2',
         'Test Description 2 of my recipe',
-        'https://bk-ca-prd.s3.amazonaws.com/sites/burgerking.ca/files/02750-2%20BK_Web_DblQtrPndKing_500x540px_CR_0.png'
-        )
+        'https://bk-ca-prd.s3.amazonaws.com/sites/burgerking.ca/files/02750-2%20BK_Web_DblQtrPndKing_500x540px_CR_0.png',
+        [
+          new Ingredient('cheese', 1),
+          new Ingredient('meatball', 1),
+          new Ingredient('bun', 2)
+        ])
       ];
 
     getRecipes() {
