2f7a476f72a3f6d12202a9434d6fb7dbc4e7d5f4
Vladislav Kavazov <vladislav.kavazov@scalefocus.com>
2019-03-16 15:26:20 +0200

    add shopping-list service

---
+++ ng7-app/src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,19 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
+
+export class ShoppingListService {
+    ingredientChanged = new EventEmitter<Ingredient[]>();
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 3)
+    ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientChanged.emit(this.ingredients.slice());
+    }
+}
